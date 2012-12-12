0ca079549abe27adf29f88fd178143230ea52acd
cdmihai <codo@illinois.edu>
2012-12-11 18:25:28 -0600

    Introduced yet another hook in CommitDiffEditFilter that filters the actual edits.

--- org.gitective.core/src/main/java/org/gitective/core/filter/commit/CommitDiffEditFilter.java
+++ org.gitective.core/src/main/java/org/gitective/core/filter/commit/CommitDiffEditFilter.java
@@ -102,6 +102,8 @@ public class CommitDiffEditFilter extends CommitDiffFilter {
 			if(!acceptDiff(diff, edits))
 				continue;
 
+			edits = filterEdits(edits);
+
 			if (!include(commit, diff, edits))
 				return markEnd(commit).include(false);
 		}
@@ -110,7 +112,17 @@ public class CommitDiffEditFilter extends CommitDiffFilter {
 	}
 
 	/**
-	 * Hook that specifies wheter to accept the specified diff or not.
+	 * Hook to filter the individual edits before handing them to {@link CommitDiffEditFilter#include(RevCommit, DiffEntry, Collection)}}.
+	 * Base implementation returns the initial edits.
+	 * @param edits
+	 * @return
+	 */
+	protected Collection<Edit> filterEdits(Collection<Edit> edits) {
+		return edits;
+	}
+
+	/**
+	 * Hook that specifies whether to accept the specified diff or not.
 	 * In case it is not accepted, the CommitDiffEditFilter won't call it's {@link CommitDiffEditFilter#include(RevCommit, DiffEntry, Collection)}} hook with this diff
 	 * @param diff - the diff to check
 	 * @param edits - the diff's edits
